4f41b618e1f33529edc7b961c75ac2d9f1a663ec
Ben Leggett <benjamin.leggett1@t-mobile.com>
2020-05-28 11:17:56 -0400

    Init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# This is me trying to learn Rust by doing a blackjack sim
+
+Trying to avoid referencing specific examples and rely entirely on stuff hosted
+at <https://doc.rust-lang.org>
+
+Goal: Get to that [magic Zen
+state](https://doc.rust-lang.org/1.8.0/book/references-and-borrowing.html#meta)
+where I'm able to think somewhat more intuitively about the borrow checker
--- main.rs
+++ main.rs
@@ -1,15 +1,23 @@
 use std::io;
+use rand::thread_rng;
+use rand::seq::SliceRandom;
 
 
 fn main() {
     println!("Hello, world!");
-    println!("{:?}", populate_deck());
+    let mut deck = populate_deck();
+    println!("{:?}", shuffle_deck(&mut deck));
+
+    let player1 = Player {name: "Bobby".to_string(), hand: vec![deck.pop().unwrap(), deck.pop().unwrap()]};
+    let player2 = Player {name: "Jess".to_string(), hand: vec![deck.pop().unwrap(), deck.pop().unwrap()]};
 }
+
 
 #[derive(Debug)]
 struct Card {
     suit: String,
-    value: i32,
+    value: i32, //the "game" value
+    index: i32, //the index of the card, 1=ace, 13 = king
 }
 
 struct Player {
@@ -21,10 +29,23 @@ fn populate_deck() -> Vec<Card> {
     let suits = vec!["Diamonds", "Hearts", "Spades", "Clubs"];
     let mut deck = Vec::new();
     for suit in suits.iter() {
-        for value in 1..13 {
-            deck.push(Card {suit: suit.to_string(), value});
+        for index in 1..13 {
+            let mut value = index;
+            //Cards over 10 have their game value capped at 10
+            if index > 10 {
+                value = 10
+            }
+            deck.push(Card {suit: suit.to_string(), value, index});
         }
     }
+
+    return deck;
+}
+
+fn shuffle_deck(deck: &mut Vec<Card>) -> &Vec<Card> {
+    let mut rng = thread_rng();
+
+    deck.shuffle(&mut rng);
 
     return deck;
 }
